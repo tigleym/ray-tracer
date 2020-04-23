cec2788ef8f860cbac94e4f3a499f1d33a9a8695
Micah Tigley <mtigley@mozilla.com>
2020-04-22 23:16:37 -0400

    Chapter 2 Exercise: Plotting pixel-to-projectile coordinates

--- src/exercises/tick.rs
+++ src/exercises/tick.rs
@@ -1,5 +1,9 @@
-// Exercise 1: Fire projectiles and see how far they go.
-use crate::tuples::{ Vector, Point };
+use std::fs;
+use std::io::Result;
+
+// Exercise 1 & 2: Fire projectiles and see how far they go.
+use crate::tuples::{ Vector, Point, Color };
+use crate::canvas::Canvas;
 
 pub struct Projectile {
   position: Point,
@@ -11,17 +15,28 @@ pub struct Environment {
   wind: Vector,
 }
 
-fn tick(env: &Environment, proj: &Projectile) -> Projectile {
+fn tick(env: &Environment, proj: &Projectile, canvas: &mut Canvas) -> Projectile {
   let position = proj.position + proj.velocity;
   let velocity = proj.velocity + env.gravity + env.wind;
+
+  let pixel_x = position.x.round() as u32;
+  let pixel_y = position.y.round() as u32;
+  let within_bounds = pixel_x < canvas.width && pixel_y < canvas.height;
+
+  if within_bounds {
+    // canvas's y-coordinate is upside down to world coordinates. Convert the projectile's
+    // coordinates to canvas coordinates by subtracting the projectile's y from the canvas
+    // height.
+    canvas.write_pixel(pixel_x, canvas.height - pixel_y, Color { r: 1.0, g: 0.5, b: 0.0 });
+  }
 
   Projectile { position, velocity }
 }
 
-pub fn fire_projectiles() {
+pub fn fire_projectiles() -> Result<()> {
   let mut p = Projectile {
     position: Point { x: 0.0, y: 1.0, z: 0.0 },
-    velocity: Vector { x: 1.0, y: 1.0, z: 0.0 }.normalize()
+    velocity: Vector { x: 1.0, y: 1.8, z: 0.0 }.normalize() * 12.5
   };
 
   let e = Environment {
@@ -29,18 +44,23 @@ pub fn fire_projectiles() {
     wind: Vector { x: -0.01, y: 0.0, z: 0.0 }
   };
 
-  println!("Starting projectile position: {:?}", p.position);
+  let mut c = Canvas::new(1200, 700);
+
   let mut ticks = 0;
 
   loop {
-    if p.position.y <= 0.0 {
-      break;
-    }
-
     println!("Projectile position: {:?}", p.position);
     println!("Number of Ticks: {}", ticks);
     ticks += 1;
 
-    p = tick(&e, &p);
+    p = tick(&e, &p, &mut c);
+
+    if p.position.y <= 0.0 {
+      break;
+    }
   }
+
+  fs::write("projectile.ppm", c.to_ppm())?;
+
+  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
+use std::io::Result;
+
 mod canvas;
 mod tuples;
 mod exercises;
 
 use exercises::fire_projectiles;
-use canvas::Canvas;
-use tuples::Color;
 
-fn main() {
-  let mut c = Canvas::new(10, 2);
+fn main() -> Result<()> {
+  fire_projectiles()?;
 
-  for row in 0..c.height {
-    for col in 0..c.width {
-      c.write_pixel(col, row, Color { r: 1.0, g: 0.8, b: 0.6});
-    }
-  }
-
-  let ppm = c.to_ppm();
-
-  println!("{}", ppm);
+  Ok(())
 }
